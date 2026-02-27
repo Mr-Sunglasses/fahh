36e6050e89efa1e7ff61886d4f289e721ca570db
Kanishk Pachauri <itskanishkp.py@gmail.com>
2026-02-27 17:24:23 +0530

    feat: add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,21 @@
 import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    AstroPWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{html,js,css,mp3}"],
+      },
+      manifest: {
+        name: "Fahh 💨",
+        short_name: "Fahh",
+        description: "Click to Fahh 💨",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+      },
+    }),
+  ],
+});
